084d4364cded83d93c2dea69d506456eb5eeb220
Joe Bordes <joe@tsolucio.com>
2020-01-18 18:24:36 +0100

    retrieve only active flow steps

--- modules/cbProcessFlow/cbProcessFlow.php
+++ modules/cbProcessFlow/cbProcessFlow.php
@@ -202,7 +202,7 @@ class cbProcessFlow extends CRMEntity {
 			from vtiger_cbprocessstep
 			inner join vtiger_crmentity on crmid=cbprocessstepid
 			inner join vtiger_cbprocessflow on processflow=cbprocessflowid
-			where deleted=0 and processflow=? and fromstep=?',
+			where deleted=0 and processflow=? and fromstep=? and active',
 			array($processflow, $fromstate)
 		);
 		$states=array();
